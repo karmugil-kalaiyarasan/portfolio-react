f394607d4a643a624e7d60399f7d7b6c8edfdfb6
karmugil-kalaiyarasan <63269889+karmugil-kalaiyarasan@users.noreply.github.com>
2022-02-10 19:27:09 -0500

    ecommerce added

--- src/data.js
+++ src/data.js
@@ -4,35 +4,47 @@ import Quiz from "./image/quizapp.png";
 import Calculator from "./image/calculator.png";
 import Rock from "./image/rockpaperscissor.png";
 import Timer from "./image/timer.png";
+import Ecommerce from "./image/ecommerce.png";
+import Admin from "./image/ecommerce-admin.png";
 
 export const products = [
   {
     id: 1,
+    img: Ecommerce,
+    link: "https://github.com/karmugil-kalaiyarasan/ecommerce-reactjs.git",
+  },
+  {
+    id: 2,
+    img: Admin,
+    link: "https://github.com/karmugil-kalaiyarasan/ecommerce-reactjs.git",
+  },
+  {
+    id: 3,
     img: Expense,
     link: "https://github.com/karmugil-kalaiyarasan/expense-tracker.git",
   },
   {
-    id: 2,
+    id: 4,
     img: Weather,
     link: "https://github.com/karmugil-kalaiyarasan/weather-app.git",
   },
   {
-    id: 3,
+    id: 5,
     img: Quiz,
     link: "https://github.com/karmugil-kalaiyarasan/quiz-app.git",
   },
   {
-    id: 4,
+    id: 6,
     img: Calculator,
     link: "https://github.com/karmugil-kalaiyarasan/calculator.git",
   },
   {
-    id: 5,
+    id: 7,
     img: Rock,
     link: "https://github.com/karmugil-kalaiyarasan/rock-paper-scissors.git",
   },
   {
-    id: 6,
+    id: 8,
     img: Timer,
     link: "https://github.com/karmugil-kalaiyarasan/Event_timer.git",
   },
